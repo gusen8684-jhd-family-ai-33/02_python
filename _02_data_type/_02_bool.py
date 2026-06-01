417b79b7998afe022718d2aa4df7e9121547df98
gusen8684 <gusen8684@gmail.com>
2026-06-01 09:41:18 +0900

    20260601_Bool(and, or)

--- _02_data_type/_02_bool.py
+++ _02_data_type/_02_bool.py
@@ -21,6 +21,58 @@ print("1 == 1:", 1 == 1)  # True
 print("1 != 1:", 1 != 1)  # False
 
 # 논리 부정 연산(not)
-print(not True)
+print(True)
 print(not True)
 print(not not True)
+
+# and  연산
+# - A가 참이고 그리고 B도 참인 경우에 참
+# T and T == T (중요!)
+# T and F == F
+# F and T == F
+# F and F == F
+
+print('---and---')
+print(100 > 0 and 1 == 1) # True
+print(30 > 20 and 123 != 123) # False
+print(3<= -3 and 12 > 12) #False
+print(9 >= 9 / 9 * 9 and 12 != 12 + 1) # True
+
+a= 9 >= 9 / 9 * 9
+b= 12 != 12 + 1
+print( a and b)
+
+# or 연산
+# A 또는 B 가 True이면 결과도 True
+#<-> A와 B가 모두 False 이면 False
+
+# T or T == T
+# T or F == T
+# F or T == T
+# F or F == F  (중요)
+print('--- or---')
+print(100 > 0 or 1 == 1) # True
+
+print(10 * 10 == 100 or 1 != 1) # True
+
+print(100 == 0 or 10 == 10)    #True
+
+print(10 + 10 * 5 == 110 or 20 / 10 + 5 == 7) #False
+
+#60점 이상 입력 시 합격(True) 아니면 불합격(False)
+print("--- 합격 / 불합격 ---")
+
+#input() 함수: 키도드 입력을 받는 함수 (str 로 저장)
+#int() 함수 : str -> int 로 변환
+score = int(input('점수를 입력하세요: '))
+print(score, type(score))
+
+result = score >= 60
+#print("합격 여부: ",result)
+print("합격 여부:", '합격 'if result == True else '불합격' )
+
+
+
+
+
+
